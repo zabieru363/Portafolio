8c3e0d8c7d1130e622e7ce7d5c7d9c95d7af8517
zabieru363 <72569208+zabieru363@users.noreply.github.com>
2022-05-19 21:06:17 +0200

    Creado el HTML de la sección mis proyectos

--- index.php
+++ index.php
@@ -67,6 +67,21 @@
             <a href="ejemplo.txt" download="ejemplo.txt" class="download-button"><i class="fa-solid fa-download"></i> Descarga mi curriculum aquí.</a>
         </section>
 
+        <!-- SECCIÓN PORTAFOLIO -->
+        <section id="portfolio">
+            <h1 class="section-title-black">Mis proyectos</h1>
+            <div class="projects">
+                <div id="project-1" class="project-box project-1">
+                    <h3>Mi página portafolio personal</h3>
+                    <p id="description-project" class="description-project">
+                        Una página que creé que sirve para explicar un poco sobre mi, las
+                        tecnologias con las que trabajo, los proyectos que he hecho, etc.
+                    </p>
+                    <a href=""><i class="fa-brands fa-github"></i> Ver proyecto</a>
+                </div>
+            </div>
+        </section>
+
         <!-- FONT AWESOME -->
         <script src="https://kit.fontawesome.com/833d921ab7.js" crossorigin="anonymous"></script>
     </body>
