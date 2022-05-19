88c69914d343d0aef853b86fe540b61705765808
zabieru363 <72569208+zabieru363@users.noreply.github.com>
2022-05-19 21:07:04 +0200

    Creado el HTML de la sección contacto.

--- index.php
+++ index.php
@@ -82,6 +82,23 @@
             </div>
         </section>
 
+        <!-- SECCIÓN CONTACTO -->
+        <section id="contact">
+            <h1 class="section-title">Contacto</h1>
+            <form class="contact-form" name="contact-form" method="POST" action="<?php echo $_SERVER["PHP_SELF"]?>">
+                <label for="name">Tu nombre</label>
+                <input type="text" name="name" id="name_box" placeholder="Ej: Antonio Garcia">
+
+                <label for="email">Tu email</label>
+                <input type="email" name="email" id="email_box" placeholder="Ej: alguien@gmail.com">
+
+                <label for="message">¿Que quieres preguntarme?</label>
+                <textarea name="message" id="message_box" cols="60" rows="10" placeholder="Dudas, cuestiones, etc."></textarea>
+
+                <input class="send-btn" type="submit" name="Send" value="Enviar mensaje">
+            </form>
+        </section>
+
         <!-- FONT AWESOME -->
         <script src="https://kit.fontawesome.com/833d921ab7.js" crossorigin="anonymous"></script>
     </body>
