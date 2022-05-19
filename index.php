49455dd0da853b7afa91c33925b84f9e4e3c8e19
zabieru363 <72569208+zabieru363@users.noreply.github.com>
2022-05-19 21:07:40 +0200

    Estructura HTML del footer creada.

--- index.php
+++ index.php
@@ -99,6 +99,16 @@
             </form>
         </section>
 
+        <!-- FOOTER -->
+        <footer>
+            <p class="copyrigth">
+                Powered by Zabieru &copy; <?= date("Y") ?>
+            </p>
+            <a href="mailto:zabierujlc@gmail.com"><i class="fa-solid fa-envelope"></i> zabierujlc@gmail.com</a>
+            <a href="https://www.instagram.com/zabieru.exe/" target="blank"><i class="fa-brands fa-instagram"></i> @zabieru.exe</a>
+            <a href="https://twitter.com/Zabieru222" target="blank"><i class="fa-brands fa-twitter"></i> Zabieru222</a>
+        </footer>
+
         <!-- FONT AWESOME -->
         <script src="https://kit.fontawesome.com/833d921ab7.js" crossorigin="anonymous"></script>
     </body>
