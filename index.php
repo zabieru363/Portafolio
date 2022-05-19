e769dd7849bfaaab9c5f01bbf32fe0056015b1ee
zabieru363 <72569208+zabieru363@users.noreply.github.com>
2022-05-19 21:34:49 +0200

    Estilos minificados añadidos al proyecto.

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1.0 maximum-scale=1.0 minimum-scale=1.0">
         <title>Mi portafolio</title>
-        <link rel="stylesheet" type="text/css" href="css/styles.css">
+        <link rel="stylesheet" type="text/css" href="css/styles.min.css">
     </head>
     <body>
         <!-- HEADER -->
