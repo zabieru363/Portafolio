8418be5917556420b81d2835c7e0d3a94fb5844e
zabieru363 <72569208+zabieru363@users.noreply.github.com>
2022-05-19 21:03:26 +0200

    Estructura HTML del navbar creada

--- index.php
+++ index.php
@@ -13,6 +13,14 @@
             <img class="image" src="img/zabieru.png" alt="foto">
             <h1>Javier López Carretero</h1>
             <p class="nickname">Zabieru</p>
+
+            <!-- NAVBAR -->
+            <nav id="navbar">
+                <a href="#flex-header"><i class="fa-solid fa-house"></i> Inicio</a>
+                <a href="#about-me"><i class="fa-solid fa-landmark"></i> Sobre mi</a>
+                <a href="#portfolio"><i class="fa-solid fa-clipboard-list"></i> Portafolio</a>
+                <a href="#contact"><i class="fa-solid fa-envelope-circle-check"></i></i> Contacto</a>
+            </nav>
         </header>
 
         <!-- FONT AWESOME -->
