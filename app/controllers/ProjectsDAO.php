f4583bf2f4ee888bb884ff1370ac9964bcdeb1b9
zabieru363 <72569208+zabieru363@users.noreply.github.com>
2022-05-21 17:39:30 +0200

    Controlador DAO para la clase de proyectos y método listar proyectos creados.

---
+++ app/controllers/ProjectsDAO.php
@@ -0,0 +1,34 @@
+<?php
+    require_once "app/models/Project.php";
+    require_once "app/config/Connection.php";
+
+    class ProjectsDAO extends Connection
+    {
+        # Array de objetos de la clase Project.
+        public $projects = [];
+
+        # MÉTODOS CRUD.
+        public function listProjects()
+        {
+            # Vaciamos el array de objetos.
+            $this->projects = [];
+
+            $query = "SELECT * FROM proyectos";
+            $param = [];
+
+            $this->data_query($query,$param);
+
+            foreach($this->rows as $row)
+            {
+                # Creamos un nuevo objeto proyecto.
+                $project = new Project();
+
+                $project->__set("Id", $row["Id"]);
+                $project->__set("Titulo", $row["Titulo"]);
+                $project->__set("Descripcion", $row["Descripcion"]);
+                $project->__set("Enlace", $row["Enlace"]);
+                $project->__set("Foto", $row["Foto"]);
+            }
+        }
+    }
+?>
